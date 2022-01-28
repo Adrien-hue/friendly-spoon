e53f2e7f84e5005c3d5f2d57f8d73f0af3538f50
Adrien_hue <adrien.houee1@gmail.com>
2022-01-28 13:39:39 +0100

    design : Add Bootstrap

--- app/view/templates/default.php
+++ app/view/templates/default.php
@@ -8,11 +8,15 @@ use App\App;
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap/bootstrap.css">
     <title><?= App::getInstance()->title ?></title>
 </head>
 <body>
 
-    <?= $content; ?>
+    <div class="container">
+        <?= $content; ?>
+    </div>
 
 </body>
+<script src="js/bootstrap/bootstrap.js"></script>
 </html>
